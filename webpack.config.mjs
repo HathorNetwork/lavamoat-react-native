4dad7949778fa4df520c98bf6b8a41d2a3169f25
André Abadesso <andre.abadesso@gmail.com>
2024-10-17 12:29:17 -0300

    feat: initial commit

---
+++ webpack.config.mjs
@@ -0,0 +1,133 @@
+import path from 'path';
+import fs from 'fs';
+import TerserPlugin from 'terser-webpack-plugin';
+import * as Repack from '@callstack/repack';
+
+const loadJSON = (_path) => JSON.parse(fs.readFileSync(new URL(_path, import.meta.url)));
+
+const appJson = loadJSON('./app.json');
+
+export default (env) => {
+  const {
+    mode = 'development',
+    context = Repack.getDirname(import.meta.url),
+    entry = './index.js',
+    platform = process.env.PLATFORM,
+    minimize = mode === 'production',
+    devServer = undefined,
+    bundleFilename = 'index.bundle',
+    sourceMapFilename = 'index.bundle.map',
+    assetsPath = undefined,
+    reactNativePath = new URL('./node_modules/react-native', import.meta.url).pathname,
+  } = env;
+  const dirname = Repack.getDirname(import.meta.url);
+
+  if (!platform) {
+    throw new Error('Missing platform');
+  }
+
+  process.env.BABEL_ENV = mode;
+
+  return {
+    mode,
+    devtool: 'source-map',
+    context,
+    entry: [
+      ...Repack.getInitializationEntries(reactNativePath, {
+        hmr: devServer && devServer.hmr,
+      }),
+      entry,
+    ],
+    resolve: {
+      ...Repack.getResolveOptions(platform),
+      alias: {
+        'react-native': reactNativePath,
+        '@babel/runtime': path.join(dirname, 'node_modules/@babel/runtime'),
+      },
+    },
+    output: {
+      path: path.join(dirname, 'build', platform),
+      publicPath: Repack.getPublicPath({ platform, devServer }),
+      filename: 'index.bundle',
+    },
+    optimization: {
+      minimize,
+      minimizer: [
+        new TerserPlugin({
+          test: /\.(js)?bundle(\?.*)?$/i,
+          extractComments: false,
+          terserOptions: {
+            format: {
+              comments: false,
+            },
+          },
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          include: [
+            /node_modules(.*[/\\])+react/,
+            /node_modules(.*[/\\])+@react-native/,
+            /node_modules(.*[/\\])+@react-navigation/,
+            /node_modules(.*[/\\])+@react-native-community/,
+            /node_modules(.*[/\\])+@expo/,
+            /node_modules(.*[/\\])+pretty-format/,
+            /node_modules(.*[/\\])+metro/,
+            /node_modules(.*[/\\])+abort-controller/,
+            /node_modules(.*[/\\])+@callstack\/repack/,
+          ],
+          use: 'babel-loader',
+        },
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: devServer && devServer.hmr ? ['module:react-refresh/babel'] : undefined,
+            },
+          },
+        },
+        {
+          test: Repack.getAssetExtensionsRegExp(Repack.ASSET_EXTENSIONS.filter((ext) => ext !== 'svg')),
+          use: {
+            loader: '@callstack/repack/assets-loader',
+            options: {
+              platform,
+              devServerEnabled: Boolean(devServer),
+              scalableAssetExtensions: Repack.SCALABLE_ASSETS,
+            },
+          },
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                native: true,
+                dimensions: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin({
+        context,
+        mode,
+        platform,
+        devServer,
+        output: {
+          bundleFilename,
+          sourceMapFilename,
+          assetsPath,
+        },
+      }),
+    ],
+  };
+};
