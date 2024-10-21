05c588f4ebec3cd8dcb12f5fabaeaf4a281a3188
André Abadesso <andre.abadesso@gmail.com>
2024-10-21 16:13:22 -0300

    feat: using static build

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+console.log('Index.js is being executed');
+
+if (global.HermesInternal) {
+  console.log('Running on Hermes engine');
+} else {
+  console.log('Not running on Hermes engine');
+}
+
+AppRegistry.registerComponent(appName, () => {
+  console.log('AppRegistry.registerComponent called');
+  return App;
+});
+
+console.log('End of index.js');
--- ios/lavanative/AppDelegate.mm
+++ ios/lavanative/AppDelegate.mm
@@ -1,26 +1,50 @@
 #import "AppDelegate.h"
+   #import <React/RCTBundleURLProvider.h>
+   #import <React/RCTRootView.h>
 
-#import <React/RCTBundleURLProvider.h>
+   @implementation AppDelegate
 
-@implementation AppDelegate
+   - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+   {
+     self.moduleName = @"lavanative";
+     self.initialProps = @{};
+     return [super application:application didFinishLaunchingWithOptions:launchOptions];
+   }
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  self.moduleName = @"lavanative";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+   - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+   {
+   #if DEBUG
+     NSLog(@"DEBUG MODE: Using RCTBundleURLProvider");
+     return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+   #else
+     NSLog(@"RELEASE MODE: Attempting to load main.jsbundle");
+     
+     // Method 1: Using pathForResource
+     NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"main" ofType:@"jsbundle"];
+     if (bundlePath) {
+       NSLog(@"Method 1 - Bundle found at path: %@", bundlePath);
+       return [NSURL fileURLWithPath:bundlePath];
+     } else {
+       NSLog(@"Method 1 - Error: main.jsbundle not found using pathForResource");
+     }
+     
+     // Method 2: Direct path construction
+     NSString *directPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"main.jsbundle"];
+     if ([[NSFileManager defaultManager] fileExistsAtPath:directPath]) {
+       NSLog(@"Method 2 - Bundle found at direct path: %@", directPath);
+       return [NSURL fileURLWithPath:directPath];
+     } else {
+       NSLog(@"Method 2 - Error: main.jsbundle not found at direct path");
+     }
+     
+     // Logging additional information
+     NSLog(@"Bundle resource paths: %@", [[NSBundle mainBundle] resourcePath]);
+     NSLog(@"All bundle file names: %@", [[NSBundle mainBundle] pathsForResourcesOfType:nil inDirectory:nil]);
+     
+     NSLog(@"CRITICAL ERROR: Unable to find main.jsbundle. App will likely crash.");
+     return nil;
+   #endif
+   }
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-@end
+   @end
+   
